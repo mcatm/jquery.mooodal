6adcd3b892c59e024b4700c67d3bbd7c9a6e470d
HAMADA, Satoshi <tkcs@pelepop.com>
2013-06-11 18:41:49 +0900

    first commit

---
+++ jquery.mooodal.js
@@ -0,0 +1,19 @@
+(function($, document, window) {
+   
+   $.fn.mooodal = function()
+   {
+      
+      var trigger = $(this);
+      
+      var defaults = {
+         
+      };
+      
+      $(this).on('click', function() {
+         alert('UUUU');
+         return false;
+      });
+      
+   };
+   
+})(jQuery, document, window);
